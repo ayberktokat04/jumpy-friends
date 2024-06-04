112741ffed59a8705d8a26489c1822e9fae415bb
XenoverseUp <xenoverseup@gmail.com>
2024-06-04 23:47:09 +0300

    merge: feat-movement

--- src/include/startScreen.hpp
+++ src/include/startScreen.hpp
@@ -2,6 +2,7 @@
 
 #include "raylib-cpp.hpp"
 
-Vector2 gameLogoPosition = (Vector2){50.f, 50.f};
-int gameLogoFontSize = 40;
+extern Vector2 gameLogoPosition;
+extern int gameLogoFontSize;
+
 void draw();
